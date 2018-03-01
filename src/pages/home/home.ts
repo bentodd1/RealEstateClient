d9c831fd471f5f60cd5d9f522169769f0bcff31b
Ben Todd <btodd@us-c02tf1vzg8wn.local>
2018-02-28 21:11:48 -0600

    Initial Commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,79 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import {MarketDetailPage} from "../market-detail/market-detail";
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  pageTitle = 'Market Health';
+  _markets = [
+    {
+      city:'Kansas City',
+      zip:'64109',
+      health:6.4
+    },
+    {
+      city:'Kansas City',
+      zip:'64110',
+      health:8
+    },
+    {
+      city:'Kansas City',
+      zip:'64134',
+      health:4
+    },
+    {
+      city:'Kansas City',
+      zip:'64155',
+      health:86.1
+    },
+    {
+      city:'Kansas City',
+      zip:'64196',
+      health:8.4
+    },
+    {
+      city:'Overland Park',
+      zip:'66207',
+      health:5.1
+    },
+    {
+      city:'Overland Park',
+      zip:'66221',
+      health:3
+    },
+    {
+      city:'Overland Parky',
+      zip:'66283',
+      health:7
+    }
+  ];
+  markets = [];
+  constructor(public navCtrl: NavController) {
+    this.initializeItems();
+  }
+
+  goToDetail(market:any){
+    this.navCtrl.push(MarketDetailPage, {market:market});
+  }
+
+  getSearchResults(ev: any) {
+    // Reset items back to all of the items
+    this.initializeItems();
+    // set val to the value of the searchbar
+    let val = ev.target.value;
+    // if the value is an empty string don't filter the items
+      if (val && val.trim() != '') {
+        this.markets = this._markets.filter((market) => {
+          return (market.city.toLowerCase().indexOf(val.toLowerCase()) > -1 || market.zip.toLowerCase().indexOf(val.toLowerCase()) > -1 );
+        });
+      }
+  }
+
+  initializeItems() {
+    this.markets = this._markets;
+  }
+
+}
---
+++ src/pages/market-detail/market-detail.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the MarketDetailPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-market-detail',
+  templateUrl: 'market-detail.html',
+})
+export class MarketDetailPage {
+  pageTitle = 'Market Detail';
+  market:any;
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    // this.checkParams();
+    this.market = {
+      cbsa:'New York-Newark-Jersey City, NY-NJ-PA',
+      size:0,
+      health:9.3453524533,
+      sfg:95.73,
+      pf:1.0234,
+      fr:0.0254,
+      fyoy:0.038965,
+      ne:0.067887,
+      d:0.09989,
+      dom:155
+    };
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad MarketDetailPage');
+  }
+
+  checkParams(){
+    this.market = this.navParams.get('market');
+    this.pageTitle = this.market.zip;
+  }
+
+}
